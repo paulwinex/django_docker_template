9457becd6ba0c0294a56547d3d0756d63451d539
paulwinex <paulwinex@gmail.com>
2017-08-29 04:26:14 -0700

    edit docker-compose

--- _build_all.sh
+++ _build_all.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # build project image
 docker build -f Dockerfile_web -t paulwinex/project .
 
@@ -5,6 +7,7 @@ docker build -f Dockerfile_web -t paulwinex/project .
 docker build -f Dockerfile_workers -t paulwinex/worker .
 
 #build scheduler image from project image
+docker build -f Dockerfile_scheduler -t paulwinex/scheduler .
 
 #build nginx 
 docker build -t paulwinex/nginx_lua ./nginx
@@ -17,5 +20,6 @@ docker-compose up -d db
 echo "Waiting 5 sec..."
 sleep 10
 docker-compose down
+
 
 
